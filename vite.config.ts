c18ffc4ddb5f2f55ac3de7ffce8327c4d3923460
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-05 10:56:13 +0000

    wip: initial generated project from wagmi

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import { fileURLToPath } from 'url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      util: 'util',
+    },
+  },
+  plugins: [react()],
+})
