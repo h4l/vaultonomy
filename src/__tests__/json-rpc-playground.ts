17f267d32d328d8c3e5b97a5794c5bef3c06ed13
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-09 06:39:25 +0000

    chore: experiment with json-rpc-2.0 lib
    Seems like this could be a reasonable way to manage message passing
    between extension components. Using chrome.runtime.Port for messaging
    would be desirable as it will notify when the endpoint is disconnected.
    But it doesn't provide request-response semantics, just message passing.
    json-rpc-2.0 can add that, plus a bit of structure. And JSON RPC is
    commonly used for Ethereum things.

---
+++ src/__tests__/json-rpc-playground.ts
@@ -0,0 +1,118 @@
+import { jest } from "@jest/globals";
+import { waitFor } from "@testing-library/dom";
+import {
+  JSONRPCClient,
+  JSONRPCServer,
+  JSONRPCServerAndClient,
+} from "json-rpc-2.0";
+
+describe("JSON RPC", () => {
+  test("client request server", async () => {
+    const server = new JSONRPCServer();
+    const client = new JSONRPCClient(async (payload) => {
+      const resp = await server.receive(payload);
+      if (resp !== null) client.receive(resp);
+    });
+
+    server.addMethod("add", ({ a, b }: { a: number; b: number }) => ({
+      answer: a + b,
+    }));
+
+    await expect(client.request("add", { a: 1, b: 3 })).resolves.toEqual({
+      answer: 4,
+    });
+  });
+
+  test("client notify server", async () => {
+    const server = new JSONRPCServer();
+    const client = new JSONRPCClient(async (payload) => {
+      const resp = await server.receive(payload);
+      if (resp !== null) client.receive(resp);
+    });
+
+    const action = jest.fn();
+    server.addMethod("someEvent", ({ a, b }: { a: number; b: number }) => {
+      action(a, b);
+    });
+    client.notify("someEvent", { a: 1, b: 3 });
+    expect(action).toHaveBeenCalledWith(1, 3);
+  });
+
+  test("bi-directional server and client", async () => {
+    const fooSide = new JSONRPCServerAndClient(
+      new JSONRPCServer(),
+      new JSONRPCClient(async (payload) => {
+        const resp = await barSide.server.receive(payload);
+        if (resp) fooSide.client.receive(resp);
+      })
+    );
+
+    const barSide = new JSONRPCServerAndClient(
+      new JSONRPCServer(),
+      new JSONRPCClient(async (payload) => {
+        const resp = await fooSide.server.receive(payload);
+        if (resp) barSide.client.receive(resp);
+      })
+    );
+
+    let subscription: NodeJS.Timer;
+    fooSide.server.addMethod(
+      "subscribe",
+      ({ a, b }: { a: number; b: number }) => {
+        let value = a;
+        subscription = setInterval(() => {
+          fooSide.client.notify("someEvent", { detail: (value += b) });
+        }, 0);
+      }
+    );
+
+    let unsubscribed = false;
+    fooSide.server.addMethod("unsubscribe", () => {
+      clearInterval(subscription);
+      unsubscribed = true;
+    });
+
+    const event = jest.fn();
+    barSide.addMethod("someEvent", async ({ detail }: { detail: number }) => {
+      event(detail);
+      if (detail > 3) {
+        await barSide.request("unsubscribe", {});
+      }
+    });
+
+    await barSide.client.request("subscribe", { a: 0, b: 2 });
+    await waitFor(() => {
+      expect(unsubscribed).toBeTruthy();
+    });
+    expect(event).toHaveBeenNthCalledWith(1, 2);
+    expect(event).toHaveBeenNthCalledWith(2, 4);
+  });
+
+  test("server error handling", async () => {
+    const errorListener = jest.fn();
+    const server = new JSONRPCServer({ errorListener });
+
+    const client = new JSONRPCClient(async (payload) => {
+      const resp = await server.receive(payload);
+      if (resp !== null) client.receive(resp);
+    });
+
+    const error = new Error("add method failed");
+    server.addMethod("add", async () => {
+      throw error;
+    });
+
+    try {
+      await client.request("add", { a: 1, b: 3 });
+      throw new Error("expected request to fail");
+    } catch (e) {
+      // The server caught and re-transmitted the error -- it's not the same obj
+      expect(e).not.toBe(error);
+      // But it retains the same message
+      expect(e).toEqual(error);
+    }
+    expect(errorListener).toHaveBeenCalled();
+  });
+});
+
+export {};
