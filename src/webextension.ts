0c671610d684eac3903df8646ca377be81e70dff
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-10 04:36:02 +0000

    wip: feat: add WebExtension compatibility import

---
+++ src/webextension.ts
@@ -0,0 +1,16 @@
+// Use @types/chrome for the WebExtension API (rather than
+// webextension-polyfill) as they're more accurate and we only want to use
+// APIs that exist in Chrome.
+interface WebExtensionGlobal {
+  chrome?: typeof chrome;
+  browser?: typeof chrome;
+}
+
+const webExtensionGlobal = globalThis as WebExtensionGlobal;
+const _browser = webExtensionGlobal.browser ?? webExtensionGlobal.chrome;
+if (!_browser) {
+  throw new Error(
+    "WebExtension API not found on browser or chrome properties of globalThis"
+  );
+}
+export const browser: typeof chrome = _browser;
