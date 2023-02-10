2ba5c45ca089b68bcf2e7df27b1d6b16d6d287ce
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-10 04:38:40 +0000

    wip: feat: WebExtension Port transport for json-rpc-2.0
    This module allows json-rpc-2.0's clint & server to communicate over a
    WebExtension Port.

---
+++ src/rpc/webextension-port-json-rpc.ts
@@ -0,0 +1,95 @@
+import {
+  JSONRPCClient,
+  JSONRPCRequest,
+  JSONRPCResponse,
+  JSONRPCServer,
+  JSONRPCServerAndClient,
+  SendRequest,
+} from "json-rpc-2.0";
+
+type Unbind = () => void;
+
+export function bindPortToJSONRPCServer({
+  port,
+  server,
+}: {
+  port: chrome.runtime.Port;
+  server: JSONRPCServer;
+}): Unbind {
+  const listener = async (message: unknown) => {
+    const response = await server.receive(message as JSONRPCRequest);
+    if (response !== null) port.postMessage(response);
+  };
+  port.onMessage.addListener(listener);
+  return () => {
+    port.onMessage.removeListener(listener);
+  };
+}
+
+export function createPortSendRequestFn(
+  port: chrome.runtime.Port
+): SendRequest<void> {
+  return async (payload) => {
+    port.postMessage(payload);
+  };
+}
+
+export function bindPortToJSONRPCClient({
+  port,
+  client,
+}: {
+  port: chrome.runtime.Port;
+  client: JSONRPCClient;
+}): Unbind {
+  let disconnected = false;
+  const unbind = (message: string) => {
+    if (disconnected) return;
+    port.onDisconnect.removeListener(onDisconnect);
+    port.onMessage.removeListener(onMessage);
+    client.rejectAllPendingRequests(message);
+    disconnected = true;
+  };
+  const onDisconnect = () => {
+    unbind("Port is disconnected");
+  };
+  const onMessage = (message: unknown) => {
+    client.receive(message as JSONRPCResponse);
+  };
+
+  port.onDisconnect.addListener(onDisconnect);
+  port.onMessage.addListener(onMessage);
+
+  return () => {
+    unbind("JSONRPCClient was unbound from Port");
+  };
+}
+
+export function bindPortToJSONRPCServerAndClient({
+  port,
+  serverAndClient,
+}: {
+  port: chrome.runtime.Port;
+  serverAndClient: JSONRPCServerAndClient;
+}): Unbind {
+  let disconnected = false;
+  const unbind = (message: string) => {
+    if (disconnected) return;
+    port.onDisconnect.removeListener(onDisconnect);
+    port.onMessage.removeListener(onMessage);
+    serverAndClient.rejectAllPendingRequests(message);
+    disconnected = true;
+  };
+  const onDisconnect = () => {
+    unbind("Port is disconnected");
+  };
+  const onMessage = (message: unknown) => {
+    serverAndClient.receiveAndSend(message);
+  };
+
+  port.onDisconnect.addListener(onDisconnect);
+  port.onMessage.addListener(onMessage);
+
+  return () => {
+    unbind("JSONRPCServerAndClient was unbound from Port");
+  };
+}
