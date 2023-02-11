80522b731e86e6e45a74a7391ff4a6e36b5c8a0e
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-11 04:20:44 +0000

    feat: support additional user data fields from page-data
    The only thing we might need but can't get is the user's display name.
    That seems to be weirdly represented as the name of a subreddit tied to
    the user (the one that holds posts made on a user profile).

--- src/reddit/__tests__/page-data.ts
+++ src/reddit/__tests__/page-data.ts
@@ -2,34 +2,121 @@ import { jest } from "@jest/globals";
 
 import {
   DEFAULT_PAGE_DATA_URL,
-  PageData,
+  InPageRedditUser,
   fetchPageData,
   parsePageJSONData,
 } from "../page-data";
 
-function pageData(): PageData {
-  return { user: { session: { accessToken: "abc-123" } } };
+function pageDataLoggedIn(): Record<string, unknown> {
+  return {
+    other: { stuff: true },
+    user: {
+      other: { stuff: true },
+      account: {
+        other: { stuff: true },
+        id: "t2_abc",
+        isGold: true,
+        accountIcon: "https://example.com/img",
+        displayText: "exampleuser",
+      },
+    },
+    session: {
+      accessToken: "abc-123",
+    },
+  };
+}
+function pageDataLoggedOut(): Record<string, unknown>[] {
+  return [
+    {
+      other: { stuff: true },
+      user: {
+        account: null,
+      },
+      session: {
+        accessToken: "abc-123",
+      },
+    },
+    {
+      other: { stuff: true },
+      user: {
+        account: null,
+        session: {},
+      },
+    },
+    {
+      other: { stuff: true },
+      user: {
+        account: null,
+        session: null,
+      },
+    },
+  ];
 }
 
-const html = `\
+const html = (pageData: Record<string, unknown>) => `\
 <!DOCTYPE html><html lang="en-US">
 <head><title>Hi</title></head>
 <body><h1>Hi</h1>
-<script id="data">window.___r = ${JSON.stringify(pageData())};</script>
+<script id="data">window.___r = ${JSON.stringify(pageData)};</script>
 <script></script>
 </html>`;
 
-test("parsePageJSONData() extracts JSON from #data element", () => {
-  expect(parsePageJSONData(html)).toStrictEqual(pageData());
+describe("parsePageJSONData()", () => {
+  test("extracts JSON from #data element", () => {
+    const data = pageDataLoggedIn();
+    expect(parsePageJSONData(html(data))).toStrictEqual(data);
+  });
 });
 
-test("fetchPageData()", async () => {
-  const fetch = jest.spyOn(globalThis, "fetch").mockResolvedValueOnce({
-    ok: true,
-    status: 200,
-    text: async () => html,
-  } as Response);
-  await expect(fetchPageData()).resolves.toStrictEqual(pageData());
-  expect(fetch).toBeCalledTimes(1);
-  expect(fetch).toBeCalledWith(DEFAULT_PAGE_DATA_URL);
+describe("fetchPageData()", () => {
+  test("provides data for logged-in user", async () => {
+    const fetch = jest.spyOn(globalThis, "fetch").mockResolvedValueOnce({
+      ok: true,
+      status: 200,
+      text: async () => html(pageDataLoggedIn()),
+    } as Response);
+
+    const expected: InPageRedditUser = {
+      userID: "t2_abc",
+      hasPremium: true,
+      accountIconURL: "https://example.com/img",
+      username: "exampleuser",
+      authToken: "abc-123",
+    };
+    await expect(fetchPageData()).resolves.toStrictEqual(expected);
+    expect(fetch).toBeCalledTimes(1);
+    expect(fetch).nthCalledWith(1, DEFAULT_PAGE_DATA_URL);
+  });
+
+  test.each(pageDataLoggedOut().map((pageData) => [html(pageData)]))(
+    "provides data for logged-out user",
+    async (htmlWithLoggedOutUser: string) => {
+      jest.spyOn(globalThis, "fetch").mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        text: async () => htmlWithLoggedOutUser,
+      } as Response);
+
+      await expect(fetchPageData()).resolves.toBeUndefined();
+    }
+  );
+
+  test.each`
+    desc                                       | html                                                | message
+    ${"data element is not present"}           | ${""}                                               | ${"page contains no #data element"}
+    ${"data element contains unexpected data"} | ${`<script id="data">sdfds</script>`}               | ${"#data element's content is not structured as expected"}
+    ${"data element contains invalid JSON"}    | ${`<script id="data">window.___r = {"foo</script>`} | ${"data element's content is not valid JSON"}
+    ${"JSON data has unexpected structure"}    | ${html({ user: { account: { displayText: 42 } } })} | ${"page #data JSON value is not structured as expected"}
+  `(
+    "throws error when $desc",
+    async (options: { html: string; message: string }) => {
+      jest.spyOn(globalThis, "fetch").mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        text: async () => options.html,
+      } as Response);
+
+      await expect(fetchPageData()).rejects.toThrow(options.message);
+    }
+  );
 });
--- src/reddit/page-data.ts
+++ src/reddit/page-data.ts
@@ -1,31 +1,70 @@
+import { z } from "zod";
+
+import { HTTPResponseError } from "../errors/http";
+
 // Every page seems to contain a script#data element containing the session data
 // we need. But some pages are bigger than others. /coins doesn't have much
-// going on, so it's smaller & faster than (say the homepage).
+// going on, so it's smaller & faster (than say the homepage).
 export const DEFAULT_PAGE_DATA_URL = "https://www.reddit.com/coins";
 
-export interface PageData {
-  user: User;
-}
-interface User {
-  session: Session;
-}
-interface Session {
-  accessToken: string;
+const PageData = z.object({
+  user: z.object({
+    account: z
+      .object({
+        id: z.string(),
+        // has Reddit Premium (via award or subscription)
+        isGold: z.boolean().default(false),
+        accountIcon: z.string().url(),
+        // username (not display name)
+        displayText: z.string(),
+      })
+      .nullish(),
+  }),
+  session: z
+    .object({
+      accessToken: z.string().nullish(),
+    })
+    .nullish(),
+});
+
+export interface InPageRedditUser {
+  userID: string;
+  username: string;
+  hasPremium: boolean;
+  accountIconURL: string;
+  authToken: string;
 }
 
 export async function fetchPageData(
   pageUrl: string = DEFAULT_PAGE_DATA_URL
-): Promise<PageData> {
-  const resp = await fetch(pageUrl);
-  if (!resp.ok) {
-    throw new Error(
-      `Request for data-containing page failed. \
-pageUrl: ${pageUrl}, response: ${resp}`
-    );
+): Promise<InPageRedditUser | undefined> {
+  const response = await fetch(pageUrl);
+  if (!response.ok) {
+    throw new HTTPResponseError(`Request for data-containing page failed`, {
+      response,
+    });
   }
-  const json = parsePageJSONData(await resp.text());
-  validatePageData(json);
-  return json;
+  const json = parsePageJSONData(await response.text());
+  let pageData;
+  try {
+    pageData = PageData.parse(json);
+  } catch (e) {
+    throw new Error(`page #data JSON value is not structured as expected`, {
+      cause: e,
+    });
+  }
+  if (!pageData.user.account || !pageData.session?.accessToken) {
+    // not logged in
+    return undefined;
+  }
+  const account = pageData.user.account;
+  return {
+    userID: account.id,
+    username: account.displayText,
+    accountIconURL: account.accountIcon,
+    hasPremium: account.isGold,
+    authToken: pageData.session.accessToken,
+  };
 }
 
 export function parsePageJSONData(html: string): object {
@@ -37,14 +76,9 @@ export function parsePageJSONData(html: string): object {
   );
   if (!dataScriptContent)
     throw new Error("#data element's content is not structured as expected");
-  return JSON.parse(dataScriptContent[1]);
-}
-
-export function validatePageData(
-  pageData: unknown
-): asserts pageData is PageData {
-  const _pageData = pageData as Partial<PageData>;
-  if (!(typeof _pageData?.user?.session?.accessToken === "string")) {
-    throw new Error(`page #data JSON value is not structured as expected`);
+  try {
+    return JSON.parse(dataScriptContent[1]);
+  } catch (e) {
+    throw new Error("#data element's content is not valid JSON", { cause: e });
   }
 }
