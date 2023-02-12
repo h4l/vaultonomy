f5325efa7ea63e3b2fd8a7c77c41c5fcd841d9f0
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-12 06:12:26 +0000

    feat: implement reddit-interaction RPC client

---
+++ src/reddit/__tests__/reddit-interaction-client.ts
@@ -0,0 +1,92 @@
+import { expect, jest } from "@jest/globals";
+import { createJSONRPCSuccessResponse } from "json-rpc-2.0";
+
+import { MockPort } from "../../__tests__/webextension.mock";
+
+import { sleep } from "../../__tests__/testing.utils";
+import { RedditProvider } from "../reddit-interaction-client";
+import { redditEIP712Challenge } from "./api-client.fixtures";
+import { loggedInUser } from "./page-data.fixtures";
+
+describe("RedditProvider()", () => {
+  describe("from(Port)", () => {
+    test.each`
+      propagateDisconnect
+      ${true}
+      ${false}
+    `(
+      'disconnects from Port when "disconnectSelf" fires with propagateDisconnect: $propagateDisconnect',
+      async ({ propagateDisconnect }: { propagateDisconnect: boolean }) => {
+        const rpDisconnected = jest.fn();
+        const port = new MockPort();
+        const reddit = RedditProvider.from(port, { propagateDisconnect });
+        reddit.emitter.on("disconnected", rpDisconnected);
+
+        reddit.emitter.emit("disconnectSelf");
+        await sleep();
+
+        expect(port.onMessage.removeListener).toHaveBeenCalled();
+        expect(jest.mocked(port.disconnect).mock.calls.length).toEqual(
+          propagateDisconnect ? 1 : 0
+        );
+        // As with Port, initiating a disconnect does not notify ourself.
+        expect(rpDisconnected).not.toHaveBeenCalled();
+      }
+    );
+
+    test("provider fires disconnected when its Port's other end disconnects", async () => {
+      const rpDisconnected = jest.fn();
+      const port = new MockPort();
+      const reddit = RedditProvider.from(port);
+      reddit.emitter.on("disconnected", rpDisconnected);
+
+      port.receiveDisconnect();
+      await sleep();
+
+      expect(rpDisconnected).toHaveBeenCalled();
+      expect(port.onMessage.removeListener).toHaveBeenCalled();
+    });
+  });
+
+  describe("RPC methods", () => {
+    let port: MockPort;
+    let reddit: RedditProvider;
+    beforeEach(() => {
+      port = new MockPort();
+      reddit = RedditProvider.from(port);
+    });
+
+    test("getUserProfile()", async () => {
+      const resp = reddit.getUserProfile();
+      port.receiveMessage(createJSONRPCSuccessResponse(1, loggedInUser().user));
+      await expect(resp).resolves.toEqual(loggedInUser().user);
+    });
+
+    test("createAddressOwnershipChallenge()", async () => {
+      const resp = reddit.createAddressOwnershipChallenge({
+        address: `0x${"0".repeat(40)}`,
+      });
+      port.receiveMessage(
+        createJSONRPCSuccessResponse(1, redditEIP712Challenge())
+      );
+      await expect(resp).resolves.toEqual(redditEIP712Challenge());
+    });
+
+    test("registerAddressWithAccount()", async () => {
+      const resp = reddit.registerAddressWithAccount({
+        address: `0x${"0".repeat(40)}`,
+        challengeSignature: `0x${"0".repeat(130)}`,
+      });
+      port.receiveMessage(createJSONRPCSuccessResponse(1, null));
+      await expect(resp).resolves.toBeNull();
+    });
+
+    test("getAccountVaultAddress()", async () => {
+      const resp = reddit.getAccountVaultAddress();
+      port.receiveMessage(
+        createJSONRPCSuccessResponse(1, `0x${"0".repeat(40)}`)
+      );
+      await expect(resp).resolves.toEqual(`0x${"0".repeat(40)}`);
+    });
+  });
+});
---
+++ src/reddit/reddit-interaction-client.ts
@@ -0,0 +1,92 @@
+import { JSONRPCClient } from "json-rpc-2.0";
+import { Emitter, createNanoEvents } from "nanoevents";
+
+import { createRCPMethodCaller } from "../rpc/typing";
+import {
+  bindPortToJSONRPCClient,
+  createPortSendRequestFn,
+} from "../rpc/webextension-port-json-rpc";
+import { RedditEIP712Challenge } from "./api-client";
+import {
+  RedditCreateAddressOwnershipChallenge,
+  RedditCreateAddressOwnershipChallengeParams,
+  RedditGetAccountVaultAddress,
+  RedditGetUserProfile,
+  RedditRegisterAddressWithAccount,
+  RedditRegisterAddressWithAccountParams,
+  RedditUserProfile,
+} from "./reddit-interaction-spec";
+
+export type EmptyCallback = () => void;
+export interface RedditProviderEvents {
+  /** Fired when the other end disconnects from us. */
+  disconnected: EmptyCallback;
+  /** Fired to disconnect ourself from the other end. */
+  disconnectSelf: EmptyCallback;
+}
+
+/** An client interface to the reddit-interaction service.
+ *
+ * Disconnect the provider from its transport by emitting `disconnectSelf`:
+ *
+ * ```typescript
+ * provider.emitter.emit("disconnectSelf");
+ * ```
+ */
+export class RedditProvider {
+  /** Create a RedditProvider that communicates over a WebExtension Port.
+   *
+   * The Port itself is also disconnected when this provider disconnects itself
+   * from the Port, unless `options.propagateDisconnect` is `false`.
+   */
+  public static from(
+    port: chrome.runtime.Port,
+    options?: { propagateDisconnect?: boolean }
+  ) {
+    const client = new JSONRPCClient(createPortSendRequestFn(port));
+    const rp = new RedditProvider({ redditInteractionClient: client });
+    const unbind = bindPortToJSONRPCClient({ port, client });
+    // propagate disconnection between the provider and the port both ways
+    rp.emitter.on("disconnectSelf", () => {
+      unbind();
+      if (options?.propagateDisconnect !== false) {
+        port.disconnect();
+      }
+    });
+    port.onDisconnect.addListener(() => {
+      rp.emitter.emit("disconnected");
+    });
+    return rp;
+  }
+
+  readonly emitter: Emitter<RedditProviderEvents> = createNanoEvents();
+
+  constructor(options: { redditInteractionClient: JSONRPCClient }) {
+    const client = options.redditInteractionClient;
+    this.getUserProfile = createRCPMethodCaller({
+      method: RedditGetUserProfile,
+      client,
+    });
+    this.createAddressOwnershipChallenge = createRCPMethodCaller({
+      method: RedditCreateAddressOwnershipChallenge,
+      client,
+    });
+    this.registerAddressWithAccount = createRCPMethodCaller({
+      method: RedditRegisterAddressWithAccount,
+      client,
+    });
+    this.getAccountVaultAddress = createRCPMethodCaller({
+      method: RedditGetAccountVaultAddress,
+      client,
+    });
+  }
+
+  getUserProfile: () => Promise<RedditUserProfile>;
+  createAddressOwnershipChallenge: (
+    params: RedditCreateAddressOwnershipChallengeParams
+  ) => Promise<RedditEIP712Challenge>;
+  registerAddressWithAccount: (
+    params: RedditRegisterAddressWithAccountParams
+  ) => Promise<null>;
+  getAccountVaultAddress: () => Promise<string | null>;
+}
