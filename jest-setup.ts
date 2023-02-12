c56e2e646781658db046f243d0c4b6d8a567c14b
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-12 06:03:40 +0000

    feat(test): add equality tester for JSON RPC Errors

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,3 +1,20 @@
-import { jest } from "@jest/globals";
+import { expect, jest } from "@jest/globals";
+import { JSONRPCErrorException } from "json-rpc-2.0";
 
 globalThis.fetch = jest.fn<typeof globalThis.fetch>();
+
+expect.addEqualityTesters([
+  // consider error code and data when comparing JSON RCP errors
+  function jsonRPCErrorExceptionsAreEqual(e1, e2) {
+    if (
+      e1 instanceof JSONRPCErrorException &&
+      e2 instanceof JSONRPCErrorException
+    ) {
+      return (
+        e1.message === e2.message &&
+        e1.code === e2.code &&
+        this.equals(e1.data, e2.data)
+      );
+    }
+  },
+]);
