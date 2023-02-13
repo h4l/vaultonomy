db2a9efb8e61ab43ed72788873de216bb5000ded
Hal Blackburn <hwtb2@cam.ac.uk>
2023-02-13 15:50:57 +0000

    wip: hack on rollup/ts config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,11 @@
-import { fileURLToPath } from "url";
-import { defineConfig, RollupOptions } from "rollup";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import merge from "deepmerge";
-import html from "@web/rollup-plugin-html";
-import copy from "rollup-plugin-copy";
-
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+// import * as html from "@web/rollup-plugin-html/index.mjs";
+import merge from "deepmerge";
+import { RollupOptions, defineConfig } from "rollup";
+import copy from "rollup-plugin-copy";
+import { fileURLToPath } from "url";
 
 const commonConfig: RollupOptions = {
   plugins: [typescript(), commonjs(), nodeResolve({ browser: true })],
@@ -45,9 +44,9 @@ export default [
         entryFileNames: "[name].js",
       },
       plugins: [
-        html({
-          input: fileURLToPath(new URL("./html/popup.html", import.meta.url)),
-        }),
+        // html({
+        //   input: fileURLToPath(new URL("./html/popup.html", import.meta.url)),
+        // }),
         copy({
           targets: [{ src: "public/manifest.json", dest: "dist/" }],
         }),
