874f4b8fc1d536ee673f7c9a12d34c6525ff414c
Hal Blackburn <hwtb2@cam.ac.uk>
2023-12-26 18:09:09 +0000

    feat: implement inline help marker
    A smaller (?) icon that shows up next to elements when the global (?)
    icon is active. When clicked, the help associated with the UI element
    shows up in the help footer (will be exposed to screen readers inline
    with the element (TODO)).

--- src/ui/Help.tsx
+++ src/ui/Help.tsx
@@ -1,6 +1,9 @@
 import {
+  ForwardedRef,
+  ReactNode,
   Reducer,
   createContext,
+  forwardRef,
   useContext,
   useEffect,
   useReducer,
@@ -89,9 +92,84 @@ export function useRootHelpState(): HelpState {
   return help;
 }
 
-export function HelpItem({ helpText }: { helpText: string }): JSX.Element {
+interface HelpProps {
+  helpId?: string;
+  helpText: string;
+}
+
+function buttonPosition(
+  containerHeight: number,
+  buttonHeight: number
+): { top: string } {
+  // Vertically-center the (?) icon. Use 90% of container height to shift the
+  // button up slightly, as we typically apply to single text lines which are
+  // visually biased towards the top of their height.
+  return { top: `${(containerHeight * 0.9 - buttonHeight) / 2}px` };
+}
+
+export const HelpButton = forwardRef(function HelpButton(
+  {
+    helpId: _helpId,
+    helpText,
+    className,
+  }: {
+    helpId?: string;
+    helpText: string;
+    className?: string;
+  },
+  ref: ForwardedRef<HTMLButtonElement>
+): JSX.Element {
+  const helpId = _helpId ?? helpText;
   const help = useContext(HelpContext);
-  return <></>;
+  const isSelected = help.selectedHelpItem?.helpId === helpId;
+
+  return (
+    <button
+      ref={ref}
+      className={`${help.helpEnabled ? "" : "hidden"}
+                  ${isSelected ? "drop-shadow" : ""}
+                  ${className || ""}`}
+      onClick={() => {
+        help.dispatch(
+          isSelected
+            ? { type: "help-item-deselected", helpId }
+            : { type: "help-item-selected", helpId, helpText }
+        );
+      }}
+    >
+      <div
+        className={`p-[0.125rem] clip-circle bg-green-700 transition-all ${
+          isSelected ? "text-neutral-100" : "bg-transparent"
+        }`}
+      >
+        <HelpIcon />
+      </div>
+    </button>
+  );
+});
+
+export function WithInlineHelp({
+  children,
+  ...props
+}: HelpProps & { children: ReactNode }): JSX.Element {
+  const container = useRef<HTMLDivElement>(null);
+  const button = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    console.log("WithInlineHelp useEffect:", container.current, button.current);
+    if (!(container.current && button.current)) return;
+    const ch = container.current.offsetHeight;
+    const bh = button.current.offsetHeight;
+    button.current.style.top = buttonPosition(ch, bh).top;
+    console.log("WithInlineHelp useEffect: top:", buttonPosition(ch, bh).top);
+  });
+
+  return (
+    <div ref={container} className="relative">
+      <HelpButton ref={button} {...props} className="absolute -left-8" />
+      {children}
+    </div>
+  );
 }
 
 function useWindowWidth(): number {
@@ -198,8 +276,9 @@ export function HelpModal(): JSX.Element {
         onTransitionEnd={() => setTransitionState("at-end")}
       >
         <button
-          className="fixed left-0 bottom-0 drop-shadow
-                   "
+          className={`fixed left-0 bottom-0 ${
+            help.helpEnabled ? "drop-shadow" : ""
+          }`}
           onClick={() => {
             help.dispatch({
               type: help.helpEnabled ? "help-disabled" : "help-enabled",
@@ -216,10 +295,42 @@ export function HelpModal(): JSX.Element {
           </div>
         </button>
         <div className="max-w-prose flex flex-col justify-center">
-          <p>The primary ENS (Ethereum Name Service) name of this address.</p>
+          {help.selectedHelpItem ? (
+            <p>{help.selectedHelpItem.helpText}</p>
+          ) : (
+            <p>
+              Click a <HelpIcon className="inline" /> to show help.
+            </p>
+          )}
         </div>
       </aside>
     </div>
+  );
+}
+
+function HelpIcon({
+  className,
+  size,
+}: {
+  size?: number | string;
+  className?: string;
+}): JSX.Element {
+  size = size ?? 24;
+  return (
+    // https://fonts.google.com/icons?selected=Material+Symbols+Outlined:help:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=help
+    <svg
+      aria-hidden="true"
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      width={size}
+    >
+      <title>Help</title>
+      <path
+        fill="currentColor"
+        d="M479-247q19.74 0 33.37-13.63Q526-274.26 526-294q0-19.74-13.63-33.37Q498.74-341 479-341q-19.74 0-33.37 13.63Q432-313.74 432-294q0 19.74 13.63 33.37Q459.26-247 479-247Zm-35-149h70q0-31.5 7.75-50T563-497q26-25 40-48.25T617-600q0-54.552-39.25-83.776Q538.5-713 484.174-713q-55.296 0-89.735 28.75T346-615l63.211 24q4.789-18 22.043-38 17.253-20 52.746-20 32 0 48 17.5t16 38.5q0 20-11.75 37T507-524q-42.5 37.5-52.75 57.75T444-396Zm36 306q-80.907 0-152.065-30.763-71.159-30.763-123.797-83.5Q151.5-257 120.75-328.087 90-399.175 90-480q0-80.907 30.763-152.065 30.763-71.159 83.5-123.797Q257-808.5 328.087-839.25 399.175-870 480-870q80.907 0 152.065 30.763 71.159 30.763 123.797 83.5Q808.5-703 839.25-631.913 870-560.825 870-480q0 80.907-30.763 152.065-30.763 71.159-83.5 123.797Q703-151.5 631.913-120.75 560.825-90 480-90Zm0-75q131.5 0 223.25-91.75T795-480q0-131.5-91.75-223.25T480-795q-131.5 0-223.25 91.75T165-480q0 131.5 91.75 223.25T480-165Zm0-315Z"
+      />
+    </svg>
   );
 }
 
