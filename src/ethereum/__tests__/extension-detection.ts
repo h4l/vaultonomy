e871c1e62fc7533206b09847c192dbdbd12ae05b
Hal Blackburn <hwtb2@cam.ac.uk>
2023-12-28 17:54:48 +0000

    feat: support detecting if an extension is installed
    We need to be able to detect if MetaMask is installed to conditionally
    enable its Connector, as @metamask/provider it doesn't gracefully handle
    a connection attempt to a missing extension.

---
+++ src/ethereum/__tests__/extension-detection.ts
@@ -0,0 +1,34 @@
+import { jest } from "@jest/globals";
+
+jest.unstable_mockModule("./src/webextension", () => {
+  return {
+    browser: {
+      runtime: {
+        sendMessage: jest.fn(),
+      },
+    },
+  };
+});
+
+const { browser } = await import("../../webextension");
+const { isExtensionInstalled } = await import("../extension-detection");
+
+describe("isExtensionInstalled", () => {
+  test("reports installed when messaging succeeds", async () => {
+    jest.mocked(browser.runtime.sendMessage).mockResolvedValueOnce(null);
+    await expect(isExtensionInstalled("installed")).resolves.toBeTruthy();
+    expect(browser.runtime.sendMessage).toHaveBeenLastCalledWith(
+      "installed",
+      null
+    );
+  });
+
+  test("reports not installed when messaging fails", async () => {
+    jest.mocked(browser.runtime.sendMessage).mockRejectedValueOnce("boom");
+    await expect(isExtensionInstalled("notinstalled")).resolves.toBeFalsy();
+    expect(browser.runtime.sendMessage).toHaveBeenLastCalledWith(
+      "notinstalled",
+      null
+    );
+  });
+});
---
+++ src/ethereum/extension-detection.ts
@@ -0,0 +1,39 @@
+import { browser } from "../webextension";
+
+// Connecting to an extension ID that is not installed fails quickly, but
+// asynchronously. Usually in a 2-3ms, but I've seen 20 once when testing.
+// If we don't get an error within this time we consider the port to be
+// connected, and therefore the extension is installed.
+// This value needs to be reasonably small, as when the extension is installed,
+// the promise can hang for arbitrary durations, and a user will always need to
+// wait this duration.
+const PORT_CONNECT_ERROR_TIMEOUT = 100;
+
+/**
+ * Check if the browser extension with the given extensionId is installed.
+ *
+ * This implementation uses cross-extension messaging to probe for the
+ * extension, and this can take ~100ms to complete, so this should be run in the
+ * background and the result cached, rather than interactively.
+ */
+export async function isExtensionInstalled(
+  extensionId: string
+): Promise<boolean> {
+  try {
+    // sendMessage can block for long periods of time, e.g. Chrome seems to not
+    // wake up the receiving extension if it's not already active.
+    await Promise.race([
+      browser.runtime.sendMessage(extensionId, null),
+      new Promise((resolve) =>
+        setTimeout(() => resolve(undefined), PORT_CONNECT_ERROR_TIMEOUT)
+      ),
+    ]);
+    // We got no error within the timeout — presume connection succeeded and
+    // therefore extension is installed.
+    return true;
+  } catch (e) {
+    // When messaging an extension ID that's not installed, Chrome rejects with:
+    // > Could not establish connection. Receiving end does not exist.
+    return false;
+  }
+}
