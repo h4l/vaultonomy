7204371ae73c4113f67c9a5ee8608f89f6962e24
Hal Blackburn <hwtb2@cam.ac.uk>
2024-01-01 17:56:37 +0000

    feat: make <Button> more flexible
    It supports changing padding, and specifying attributes for things like
    click handlers.

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,12 +1,16 @@
 export function Button({
   children,
   className,
+  paddingClassName,
+  ...props
 }: {
   className?: string;
+  paddingClassName?: string;
   children: React.ReactNode;
-}): JSX.Element {
+} & JSX.IntrinsicElements["button"]): JSX.Element {
   return (
     <button
+      {...props}
       className={`border rounded-md border-neutral-200 border-l-neutral-300 border-b-neutral-300
         dark:border-neutral-800 dark:border-l-neutral-750 dark:border-b-neutral-750
       bg-neutral-50 dark:bg-neutral-900
@@ -14,7 +18,7 @@ export function Button({
         active:bg-white dark:active:bg-neutral-850
       hover:border-neutral-300 hover:border-l-neutral-400 hover:border-b-neutral-400 dark:hover:border-neutral-750 dark:hover:border-l-neutral-700 dark:hover:border-b-neutral-700
       active:shadow-solid-bottomleft active:shadow-neutral-500 dark:active:shadow-neutral-600
-      px-7 py-5 pt-[1.375]
+      ${paddingClassName ?? "px-7 py-5 pt-[1.375rem]"}
       text-4xl italic ${className || ""}`}
     >
       {children}
