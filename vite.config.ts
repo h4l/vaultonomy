11c09f7e41e4c695f501623b70d29748d42c819d
Hal Blackburn <hwtb2@cam.ac.uk>
2024-01-21 09:43:55 +0000

    build: suppress vite warning about builtin util import

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import replace from "@rollup/plugin-replace";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      util: path.resolve(__dirname, "src/polyfills/util.ts"),
+    },
+  },
   build: {
     minify: false,
     lib: {
