41088e09720b4db7a712499478305e749236c161
Hal Blackburn <hwtb2@cam.ac.uk>
2024-01-27 05:55:11 +0000

    feat: give date an accessible tooltip with full date

--- src/ui/RelativeTime.tsx
+++ src/ui/RelativeTime.tsx
@@ -1,4 +1,5 @@
 import { DateTime } from "luxon";
+import { Children, ReactNode, useId } from "react";
 
 import {
   formatAbsoluteDateTime,
@@ -9,11 +10,41 @@ export function RelativeTime({ when }: { when: number }): JSX.Element {
   const relative = useFormatDateTimeRelativeToNow(when);
 
   return (
-    <time
-      title={formatAbsoluteDateTime({ when })}
-      dateTime={DateTime.fromMillis(when).toISO() ?? undefined}
-    >
-      {relative}
+    <time dateTime={DateTime.fromMillis(when).toISO() ?? undefined}>
+      <TextWithTooltip
+        ttLabel="Full Date"
+        ttText={formatAbsoluteDateTime({ when })}
+      >
+        {relative}
+      </TextWithTooltip>
     </time>
+  );
+}
+
+function TextWithTooltip({
+  children,
+  ttLabel,
+  ttText,
+}: {
+  children?: ReactNode;
+  ttLabel: string;
+  ttText: string;
+}): JSX.Element {
+  const id = useId();
+  return (
+    <>
+      {/* SR reads the title attribute, which is annoying, as it's intended to
+          be optional/additional verbose detail. So only apply the title to the
+          SR hidden version, and provide an accessible sr-only representation. */}
+      <span className="sr-only">
+        <span aria-describedby={id}>{children}</span>
+        <span id={id} aria-label={ttLabel} role="tooltip">
+          {ttText}
+        </span>
+      </span>
+      <span aria-hidden="true" title={ttText}>
+        {children}
+      </span>
+    </>
   );
 }
