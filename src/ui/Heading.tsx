b0744a6962c9134414267decbb1fb5ca816769ac
Hal Blackburn <hwtb2@cam.ac.uk>
2024-02-07 06:22:46 +0000

    feat: allow headings' style to vary from number
    In some situations having a heading be smaller than it would normally be
    for its heading level number makes sense. The <Heading> component can
    now do this.

--- src/ui/Heading.tsx
+++ src/ui/Heading.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
-type Level = 2 | 3 | 4;
+type Level = 2 | 3 | 4 | 5;
 
 const DEFAULT_CLASSES: Record<Level, string> = {
   2: "my-8 text-5xl font-semibold",
   3: "my-6 text-4xl font-semibold",
   4: "my-6 text-3xl font-semibold",
+  5: "my-3 text-2xl font-semibold",
 };
 
 export function Heading({
@@ -14,17 +15,20 @@ export function Heading({
   className,
   children,
   level: _level,
+  visualLevel: _visualLevel,
 }: {
   id?: string;
   className?: string;
   children: ReactNode;
   level?: Level;
+  visualLevel?: Level;
 }) {
   const level = _level ?? 2;
+  const visualLevel = _visualLevel ?? level;
   const H = `h${level}` as keyof JSX.IntrinsicElements;
 
   return (
-    <H id={id} className={twMerge(DEFAULT_CLASSES[level], className)}>
+    <H id={id} className={twMerge(DEFAULT_CLASSES[visualLevel], className)}>
       {children}
     </H>
   );
