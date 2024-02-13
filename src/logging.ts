cce4e6846aa2d299520a0b454d81098f2a988c6c
Hal Blackburn <hwtb2@cam.ac.uk>
2024-02-13 08:51:49 +0000

    fix: set log level consistently
    The default level doesn't seem to apply in the reddit bundle, but
    explicitly setting the level of the default logger does.

--- src/logging.ts
+++ src/logging.ts
@@ -1,4 +1,6 @@
 import { default as _log } from "loglevel";
 
 export const log = _log;
-log.setDefaultLevel(import.meta.env.MODE === "development" ? "TRACE" : "INFO");
+const defaultLevel = import.meta.env.MODE === "development" ? "TRACE" : "INFO";
+log.setDefaultLevel(defaultLevel);
+log.setLevel(defaultLevel);
