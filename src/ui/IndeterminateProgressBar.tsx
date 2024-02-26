b04ecfcc2a163e79c8973c9c847c78c89c0e4ed2
Hal Blackburn <hwtb2@cam.ac.uk>
2024-02-26 17:36:21 +0000

    feat: support reversing the IndeterminateProgressBar
    This gives a way to provide different progress feedback for different
    stages of loading.

--- src/ui/IndeterminateProgressBar.tsx
+++ src/ui/IndeterminateProgressBar.tsx
@@ -1,7 +1,9 @@
 export function IndeterminateProgressBar({
   className,
+  reverse = false,
 }: {
   className?: string;
+  reverse?: boolean;
 }): JSX.Element {
   return (
     <div
@@ -9,8 +11,12 @@ export function IndeterminateProgressBar({
       className={`w-full h-1 bg-neutral-200 dark:bg-neutral-800 overflow-hidden relative
                   ${className ?? ""}`}
     >
-      <div className="absolute left-0 top-0 bg-neutral-300 dark:bg-neutral-700 w-full h-full origin-left scale-x-0 animate-indeterminate-progress-1" />
-      <div className="absolute left-0 top-0 bg-neutral-400 dark:bg-neutral-600 w-full h-full origin-left scale-x-0 animate-indeterminate-progress-2 animate-delay-800" />
+      <div
+        className={`absolute inset-y-0 inset-x-0 bg-neutral-300 dark:bg-neutral-700 origin-left scale-x-0 animate-indeterminate-progress-1 ${reverse ? "direction-reverse" : ""}`}
+      />
+      <div
+        className={`absolute inset-y-0 inset-x-0 bg-neutral-400 dark:bg-neutral-600 origin-left scale-x-0 animate-indeterminate-progress-2 animate-delay-800 ${reverse ? "direction-reverse" : ""}`}
+      />
     </div>
   );
 }
