175fe84198ec52afec07e2ed22557ad9d460c3bc
Hal Blackburn <hwtb2@cam.ac.uk>
2024-03-18 07:44:28 +0000

    feat: stop the reddit interaction content script
    The Reddit interaction server that runs as a content script in a Reddit
    tab now auto-stops after a short period of inactivity.
    
    This is necessary to clean up after stopping/uninstalling the extension
    (Chrome seems to not actively stop content scripts when disabling
    extensions), and also acts as a safeguard against wastefully running too
    many instances (although we should only run one at once, as the
    background service attempts to reuse the same tab).

--- src/reddit.ts
+++ src/reddit.ts
@@ -2,12 +2,15 @@ import { assert } from "./assert";
 import { log } from "./logging";
 import { createServerSession } from "./reddit/reddit-interaction-server";
 import { REDDIT_INTERACTION_PORT_NAME } from "./reddit/reddit-interaction-spec";
-import { ConnectionOverlay } from "./reddit/ui/connection-overlay";
 import { bindPortToJSONRPCServer } from "./rpc/webextension-port-json-rpc";
+import { Stop } from "./types";
 import { browser } from "./webextension";
 import { retroactivePortDisconnection } from "./webextensions/retroactivePortDisconnection";
 
-type Stop = () => void;
+// We stop the content script if there are 0 connections for this long. Note
+// that this only applies after the background service worker exits, and that
+// only exists after about 30s of inactivity.
+const idleTime = 1000 * 15;
 
 let stop: Stop | undefined = undefined;
 
@@ -15,31 +18,53 @@ function isStarted(): boolean {
   return stop !== undefined;
 }
 
-function start(): Stop {
+function start(onIdle: () => void): Stop {
   if (isStarted()) {
     assert(false, "attempted to start() when already started");
   }
 
-  const stopRedditInteractionConnections = handleRedditInteractionConnections();
+  const notifyIdleSoon = () => {
+    assert(cancelIdleNotify === undefined);
+    const notifyIdleTimeout = setTimeout(() => {
+      log.debug(
+        `Server became idle after no connections for ${idleTime / 1000}s`,
+      );
+      onIdle();
+    }, idleTime);
 
-  // TODO: remove this in due course, or at least make it non-modal so the page
-  // can still be used.
-  // TODO: introduce a timeout to close the connection if unused for some period?
-  // This may not be necessary as the background service worker will itself stop
-  // after 30s or so.
-  const connectionOverlay = new ConnectionOverlay({
-    onRemoved: () => stop && stop(),
-  });
-  connectionOverlay.render();
+    return () => {
+      cancelIdleNotify = undefined;
+      clearTimeout(notifyIdleTimeout);
+    };
+  };
+
+  let cancelIdleNotify: Stop | undefined;
+
+  const stopRedditInteractionConnections = handleRedditInteractionConnections(
+    (count) => {
+      if (count === 0) {
+        cancelIdleNotify = notifyIdleSoon();
+      } else {
+        assert(count > 0);
+        if (cancelIdleNotify) {
+          cancelIdleNotify();
+          cancelIdleNotify = undefined;
+        }
+      }
+    },
+  );
 
   return () => {
-    stop = undefined;
+    if (cancelIdleNotify) cancelIdleNotify();
     stopRedditInteractionConnections();
-    connectionOverlay.remove();
   };
 }
 
-export function handleRedditInteractionConnections(): Stop {
+export function handleRedditInteractionConnections(
+  connectionCountChanged: (count: number) => void,
+): Stop {
+  let connectionCount = 0;
+
   const onConnect = (port: chrome.runtime.Port): void => {
     log.debug("Port Connected:", port.name);
     retroactivePortDisconnection.register(port);
@@ -50,24 +75,45 @@ export function handleRedditInteractionConnections(): Stop {
     }
 
     log.debug("Starting JSONRPC server for port", port);
+    connectionCountChanged(++connectionCount);
+
     const server = createServerSession();
     bindPortToJSONRPCServer({ port, server });
     retroactivePortDisconnection.addRetroactiveDisconnectListener(port, () => {
       log.debug("Stopping JSONRPC server for port", port);
+      connectionCountChanged(--connectionCount);
     });
   };
+
   browser.runtime.onConnect.addListener(onConnect);
-  return () => browser.runtime.onConnect.removeListener(onConnect);
+  connectionCountChanged(connectionCount);
+
+  return () => {
+    browser.runtime.onConnect.removeListener(onConnect);
+  };
+}
+
+function ensureStarted(): void {
+  if (isStarted()) {
+    log.debug("start when already started, nothing to do");
+    return;
+  }
+  stop = start(() => {
+    assert(stop);
+    stop();
+    stop = undefined;
+    log.debug("Stopped idle reddit interaction server");
+  });
 }
 
 export function main() {
   if (document.readyState === "complete") {
-    stop = start();
+    ensureStarted();
     return;
   } else {
     document.addEventListener("readystatechange", () => {
       if (document.readyState === "complete") {
-        stop = start();
+        ensureStarted();
       }
     });
   }
