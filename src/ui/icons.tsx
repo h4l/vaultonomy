40cef207146d28280ceb61273ed83aa7742e62c2
Hal Blackburn <hwtb2@cam.ac.uk>
2024-03-20 08:23:32 +0000

    feat: add recommend icon

--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -206,6 +206,26 @@ export function BlockIcon(props: {
   );
 }
 
+export function RecommendIcon(props: {
+  title?: string;
+  size?: number;
+  className?: string;
+}): JSX.Element {
+  return (
+    // https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Arecommend%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024
+    <SvgIcon
+      title={props.title ?? "Recommend"}
+      {...props}
+      icon24={
+        <path
+          fill="currentColor"
+          d="M360-240h220q17 0 31.5-8.5T632-272l84-196q2-5 3-10t1-10v-32q0-17-11.5-28.5T680-560H496l24-136q2-10-1-19t-10-16l-29-29-184 200q-8 8-12 18t-4 22v200q0 33 23.5 56.5T360-240ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+        />
+      }
+    />
+  );
+}
+
 /**
  * `padding` adds padding around the logo consistent with the Google Fonts
  * icons.
