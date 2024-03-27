da1fcdd484e9cbe4fc19427a9778d01f64073564
Hal Blackburn <hwtb2@cam.ac.uk>
2024-03-27 06:15:04 +0000

    feat: use query to drive reddit availability
    We now use TanStack query to fetch/hold/update the availability state of
    Reddit tabs. This should be more reliable, as we can pro-actively poll
    it through TS Query's re-fetch on window focus behaviour, as well as
    auto set the query value from background notifications.
    
    We were doing similar things before by manually storing a value in the
    store state, but weren't re-fetching the state on focus.

---
+++ src/ui/hooks/useRedditTabAvailability.ts
@@ -0,0 +1,49 @@
+import { queryOptions, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+import { assert, assertUnreachable } from "../../assert";
+import { RedditTabAvailability } from "../../vaultonomy-rpc-spec";
+import { VaultonomyBackgroundProvider } from "../rpc/VaultonomyBackgroundProvider";
+import { useVaultonomyStoreSingle } from "../state/useVaultonomyStore";
+
+export function getRedditTabAvailabilityQueryKey() {
+  return ["VaultonomyBackgroundProvider", "RedditTabAvailability"];
+}
+
+export function getRedditTabAvailabilityQueryOptions({
+  provider,
+}: {
+  provider: VaultonomyBackgroundProvider | null;
+}) {
+  return queryOptions<RedditTabAvailability>({
+    queryKey: getRedditTabAvailabilityQueryKey(),
+    async queryFn() {
+      assert(provider, "queryFn called when not enabled");
+      return await provider.getRedditTabAvailability();
+    },
+    enabled: !!provider,
+  });
+}
+
+export function useRedditTabAvailability() {
+  const provider = useVaultonomyStoreSingle((s) => s.provider);
+  return useQuery(getRedditTabAvailabilityQueryOptions({ provider }));
+}
+
+/**
+ * Monitor a VaultonomyBackgroundProvider for Reddit tab availability changes,
+ * and update the useRedditTabAvailability query to match.
+ */
+export function useSyncRedditTabAvailabilityWithProviderNotifications() {
+  const queryClient = useQueryClient();
+  const provider = useVaultonomyStoreSingle((s) => s.provider);
+
+  useEffect(() => {
+    const stop = provider?.emitter.on("availabilityStatus", (event) => {
+      queryClient.setQueryData(getRedditTabAvailabilityQueryKey(), {
+        available: event.type === "redditTabBecameAvailable",
+      });
+    });
+    return stop;
+  }, [provider]);
+}
