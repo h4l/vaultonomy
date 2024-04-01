7b1552d554b2430f8b542e85b1ac913dcd4c56d3
Hal Blackburn <hwtb2@cam.ac.uk>
2024-04-01 07:31:50 +0000

    feat: rate-limit writes to chrome.storage.sync
    SyncedPropertiesStore now throttles writes so that users can't trigger
    the max writes limits imposed by browsers. Hitting these is possible
    now that we expose keyboard commands to change the settings which are
    written here.

--- src/settings/SyncedPropertiesStore.ts
+++ src/settings/SyncedPropertiesStore.ts
@@ -1,3 +1,5 @@
+import type { DebouncedFunc } from "lodash";
+import { default as debounce } from "lodash.debounce";
 import { Emitter, createNanoEvents } from "nanoevents";
 import { z } from "zod";
 
@@ -16,6 +18,9 @@ const log = _log.getLogger("settings/SyncedPropertiesStore");
  *
  * We prefix stored keys to avoid conflicts with other data stored in
  * browser.storage.sync.
+ *
+ * Writes to storage are rate-limited to prevent users triggering synced storage
+ * write limits.
  */
 export class SyncedPropertiesStore<PropertiesSchemaT extends z.ZodObject<any>> {
   readonly emitter: Emitter<{
@@ -29,6 +34,15 @@ export class SyncedPropertiesStore<PropertiesSchemaT extends z.ZodObject<any>> {
   readonly keyPrefix: string;
 
   private partialSchema: z.ZodObject<any>;
+
+  // Writes to the synced storage are throttled at a level that the user can
+  // hit using command shortcuts. To prevent the user triggering the limits, we
+  // cache data in-memory and flush to the synced storage with a delay, using
+  // debounce to merge writes that happen close together.
+
+  /** The cache holds un-prefixed entries. */
+  private cache: Partial<z.infer<PropertiesSchemaT>> | undefined;
+  private syncCacheSoon: DebouncedFunc<() => void>;
 
   constructor({
     schema,
@@ -45,6 +59,11 @@ export class SyncedPropertiesStore<PropertiesSchemaT extends z.ZodObject<any>> {
 
     this.partialSchema = this.schema.partial();
     this.onSyncStorageChanged = this.onSyncStorageChanged.bind(this);
+
+    this.syncCacheSoon = debounce(this.syncCacheToStorage.bind(this), 5000, {
+      leading: false,
+      trailing: true,
+    });
   }
 
   get isStarted(): boolean {
@@ -62,12 +81,15 @@ export class SyncedPropertiesStore<PropertiesSchemaT extends z.ZodObject<any>> {
     );
     const result = this.partialSchema.safeParse(changedValues);
     if (!result.success) return;
-    for (const key in result.data) {
-      if (result.data[key] === undefined) delete result.data[key];
-    }
-    if (Object.keys(result.data).length === 0) return;
-    log.debug("SyncedPropertiesStore propertiesChanged", result.data);
+
+    const changes = this.updateCache(result.data);
+
+    log.debug("SyncedPropertiesStore propertiesChanged", changes);
+    if (Object.keys(changes).length === 0) return;
+    log.debug("propertiesChanged from storage.sync.onChanged", changes);
     this.emitter.emit("propertiesChanged", result.data);
+    // Note: we don't need to call syncCacheSoon() as the changes are already in
+    // storage.sync.
   }
 
   #isStarted: boolean = false;
@@ -80,12 +102,25 @@ export class SyncedPropertiesStore<PropertiesSchemaT extends z.ZodObject<any>> {
 
   stop() {
     if (!this.#isStarted) return;
+    this.syncCacheSoon.flush();
     this.#isStarted = false;
 
     browser.storage.sync.onChanged.removeListener(this.onSyncStorageChanged);
   }
 
   async getProperties(): Promise<z.infer<PropertiesSchemaT>> {
+    const properties = { ...(await this.readFromStorage()) };
+
+    for (const key in this.cache) {
+      const value = this.cache[key];
+      if (value !== undefined) properties[key] = value;
+    }
+
+    log.debug("getProperties", properties);
+    return properties;
+  }
+
+  private async readFromStorage(): Promise<z.infer<PropertiesSchemaT>> {
     const keys = Object.keys(this.schema.shape).map(
       (k) => `${this.keyPrefix}${k}`,
     );
@@ -103,14 +138,52 @@ export class SyncedPropertiesStore<PropertiesSchemaT extends z.ZodObject<any>> {
   async setProperties(
     properties: Partial<z.infer<PropertiesSchemaT>>,
   ): Promise<void> {
+    const changes = this.updateCache(properties);
+    this.syncCacheSoon();
+    if (Object.keys(changes).length === 0) return;
+    log.debug("propertiesChanged from setProperties", changes);
+    this.emitter.emit("propertiesChanged", changes);
+  }
+
+  private updateCache(
+    properties: Partial<z.infer<PropertiesSchemaT>>,
+  ): Partial<z.infer<PropertiesSchemaT>> {
+    if (this.cache === undefined) this.cache = {};
     const parsed = this.partialSchema.parse(properties);
-    const stored = Object.fromEntries(
-      Object.entries(parsed).flatMap(([k, v]) => {
-        if (v === undefined) return [];
-        return [[`${this.keyPrefix}${k}`, v]];
-      }),
-    );
-    log.debug("set properties", stored);
-    await browser.storage.sync.set(stored);
+
+    const changes = Object.entries(parsed).flatMap(([k, v]) => {
+      if (v === undefined || this.cache![k] === v) return [];
+      return [[k, v]];
+    });
+    log.debug("set properties", properties, "changes:", changes);
+    if (changes.length === 0) return {};
+    const changesObject = Object.fromEntries(changes);
+    Object.assign(this.cache, changesObject);
+    return changesObject;
+  }
+
+  private syncCacheToStorage(): void {
+    this.#syncCacheToStorage()
+      .then((changes) => {
+        log.debug("synced cache to storage.sync, changes:", changes);
+      })
+      .catch((error) => {
+        log.error("failed to sync cache to storage.sync", error);
+      });
+  }
+
+  async #syncCacheToStorage(): Promise<Record<string, any>> {
+    if (this.cache === undefined) return {};
+
+    const stored = await this.readFromStorage();
+    const changes: Record<string, any> = {};
+    for (const key in this.cache) {
+      const cacheValue = this.cache[key];
+      if (cacheValue === undefined || cacheValue === stored[key]) continue;
+      changes[`${this.keyPrefix}${key}`] = cacheValue;
+    }
+
+    await browser.storage.sync.set(changes);
+    return changes;
   }
 }
