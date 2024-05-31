a05ee6082798052d2f223904ddb2edc99166ec2e
Hal Blackburn <hwtb2@cam.ac.uk>
2024-05-31 07:44:51 +0000

    chore: load VAULTONOMY_DEV_EXTENSION_ID from environment
    The dev mode extension ID varies by filesystem path, so it needs to be
    configurable.

--- src/devserver/constants.ts
+++ src/devserver/constants.ts
@@ -1,2 +1,10 @@
-// TODO: load from import.meta.env
-export const VAULTONOMY_DEV_EXTENSION_ID = "hdeofaehcpeliiaooagbaokoklljghoc";
+function isValidExtensionId(value: unknown) {
+  return typeof value === "string" && /\w+/.test(value);
+}
+
+if (!isValidExtensionId(import.meta.env.VITE_VAULTONOMY_DEV_EXTENSION_ID)) {
+  throw new Error("VITE_VAULTONOMY_DEV_EXTENSION_ID is not set in a .env file");
+}
+
+export const VAULTONOMY_DEV_EXTENSION_ID: string = import.meta.env
+  .VITE_VAULTONOMY_DEV_EXTENSION_ID;
