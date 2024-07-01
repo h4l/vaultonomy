23952c1e4329214612b66b674c2c343c2d17a0b9
Hal Blackburn <hwtb2@cam.ac.uk>
2024-07-01 05:14:48 +0000

    chore: strictly verify UI storage values on read
    Previously we could return non-string values from
    ExtensionAsyncStorage.getItem. This would only occur if storage became
    corrupted. We now log and ignore invalid types read from storage.

--- src/ui/state/ExtensionAsyncStorage.ts
+++ src/ui/state/ExtensionAsyncStorage.ts
@@ -1,14 +1,26 @@
 import { AsyncStorage } from "@tanstack/react-query-persist-client";
 import { StateStorage } from "zustand/middleware";
 
+import { log } from "../../logging";
 import { StorageAreaGetSetRemove } from "../../webextension";
 
 // Happily, zustand and tanstack-query's async storage interface is the same 🎉
 export class ExtensionAsyncStorage implements AsyncStorage, StateStorage {
   constructor(private storageArea: StorageAreaGetSetRemove) {}
-  async getItem(name: string) {
-    const result = await this.storageArea.get(name);
-    return result[name] ?? null;
+  async getItem(name: string): Promise<string | null> {
+    const result = (await this.storageArea.get(name))[name];
+    // webextension storage can hold any JSON value, but zustand/tanstack-query
+    // do their own serialisation and expect storage to write strings.
+    if (typeof result !== "string") {
+      if (result) {
+        log.error(
+          "Unexpected non-string value in zustand storage, value will be ignored",
+          result,
+        );
+      }
+      return null;
+    }
+    return result;
   }
   async setItem(name: string, value: string) {
     await this.storageArea.set({ [name]: value });
