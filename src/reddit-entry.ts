2b7dc1fd0aaca4bda1c76a891b459d161c69b127
Hal Blackburn <hwtb2@cam.ac.uk>
2024-07-08 06:47:08 +0000

    chore: remove old commented code

--- src/reddit-entry.ts
+++ src/reddit-entry.ts
@@ -1,27 +1,3 @@
 import { main } from "./reddit";
 
 export default main;
-// import browser from "webextension-polyfill";
-
-// console.log("reddit-entry.ts");
-// // window.open(
-// //   "chrome-extension://hdeofaehcpeliiaooagbaokoklljghoc/html/popup.html"
-// // );
-// // browser.windows.create({
-// //   url: "/html/popup.html",
-// // });
-// // Enable navigation prompt
-// window.onbeforeunload = function () {
-//   console.log("onbeforeunload");
-//   return true;
-// };
-
-// setInterval(() => {
-//   console.log("heartbeat");
-//   browser.runtime.sendMessage({
-//     msg: "heartbeat",
-//     page: globalThis.location.href,
-//   });
-// }, 1000);
-
-// export {};
