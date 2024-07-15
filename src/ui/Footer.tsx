e66b2c75184294c20a39951458505cba59f8e8f9
Hal Blackburn <hwtb2@cam.ac.uk>
2024-07-15 07:58:16 +0000

    feat: improve footer links
    We now link to the r/vaultonomy subreddit. The links have titles on
    hover, and we link to the Chrome Web Store & Firefox Add-ons.

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,8 +1,13 @@
 import { LinkButton } from "./Button";
 import { Link } from "./Link";
 import { VaultonomyLogo } from "./VaultonomyLogo";
-import { GitHubLogo, RecommendIcon, ScoreIcon } from "./icons";
+import { GitHubLogo, RecommendIcon, RedditIconMono, ScoreIcon } from "./icons";
 import { useVaultonomyStore } from "./state/useVaultonomyStore";
+
+const campaignParams =
+  "utm_medium=in-app&utm_source=vaultonomy&utm_campaign=footer-review-cta";
+const footerReviewCTAChromeWebStoreUrl = `https://chromewebstore.google.com/detail/vaultonomy/jchkniacnpkclejcbhajgajhjbgnfagi?${campaignParams}`;
+const footerReviewCTAFirefoxAddOnsUrl = `https://addons.mozilla.org/firefox/addon/vaultonomy/?${campaignParams}`;
 
 export function Footer(): JSX.Element {
   const { statsConsent, setStatsConsent } = useVaultonomyStore((s) => ({
@@ -19,29 +24,51 @@ export function Footer(): JSX.Element {
       ].join(" ")}
     >
       <div className="flex justify-end">
-        <div className="flex shrink">
+        <div className="flex gap-4 shrink">
           <Link
+            title="Vaultonomy's code on GitHub"
             href="https://github.com/h4l/vaultonomy"
-            className="flex flex-wrap gap-2 justify-center items-center text-center"
+            className="flex flex-wrap gap-1 justify-center items-center text-center"
           >
             <GitHubLogo className="w-6 basis-6 -translate-y-[0.1rem]" />
             <span>Open-source</span>
           </Link>
+          <Link
+            title="The Vaultonomy subreddit"
+            href="https://www.reddit.com/r/vaultonomy"
+            className="flex flex-wrap gap-1 justify-center items-center text-center"
+          >
+            <RedditIconMono className="w-6 basis-6 -translate-y-[0.1rem]" />
+            <span>r/vaultonomy</span>
+          </Link>
         </div>
       </div>
-      <div>
-        <Link href="https://vaultonomy.eth.limo" className="flex flex-col">
-          vaultonomy.eth.limo
-          <VaultonomyLogo className="w-32 mt-2" />
+      <div className="text-center">
+        <Link
+          href="https://app.ens.domains/vaultonomy.eth"
+          className="flex flex-col"
+        >
+          vaultonomy.eth
         </Link>
+        <VaultonomyLogo className="w-32 mt-2" />
       </div>
       <div className="flex justify-start">
-        <div className="flex gap-2 shrink">
-          {/* TODO: link to Chrome / Mozilla store */}
-          <div className="flex flex-wrap gap-1 justify-center items-center text-center leading-tight">
+        <div className="flex gap-4 shrink">
+          <Link
+            {...(VAULTONOMY.browserTarget === "chrome" ?
+              {
+                href: footerReviewCTAChromeWebStoreUrl,
+                title: "Vaultonomy on Chrome Web Store",
+              }
+            : {
+                href: footerReviewCTAFirefoxAddOnsUrl,
+                title: "Vaultonomy on Firefox Add-ons",
+              })}
+            className="flex flex-wrap gap-1 justify-center items-center text-center leading-tight"
+          >
             <RecommendIcon className="w-full basis-6 -translate-y-[0.1rem]" />
             <div>Leave a review</div>
-          </div>
+          </Link>
           <LinkButton
             onClick={() => setStatsConsent(null)}
             className="flex flex-wrap gap-1 justify-center items-center text-center leading-tight"
